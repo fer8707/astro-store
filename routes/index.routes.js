238818fb343b26c59c981376b74bf8b662b3948b
Fernando Tavera <fer8707@gmail.com>
2021-05-19 21:57:55 -0500

    add style

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,14 +5,8 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res) => {
-  let data;
-  if(res.locals.currentUser){
-    data = "Hay un usuario conectado"
-  } else{
-    data = "No hay un usuario"
-  }
-  res.render('index', { title: data }
-  )
+  
+  res.render('index')
 });
 
 module.exports = router;
